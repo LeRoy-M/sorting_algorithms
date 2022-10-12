7d923da837aea4dda4fa00832c7e334356c49074
LeRoy-M <lmwasela@gmail.com>
2022-10-12 10:15:27 -0700

    0. Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,11 +12,11 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, k, temp;
+	unsigned int i, j, k, temp;
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < (size - 1); j++)
+		for (j = 0; j < (size - 1 - i); j++)
 		{
 			if (array[j] > array[j + 1])
 			{
